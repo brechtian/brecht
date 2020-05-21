45cde02e1109f8bedb3a991e25dd631a0dfd88c8
Sebastian Harko <sebastian.harko@protonmail.com>
2020-05-20 22:00:54 -0700

    attempting to fix travis build

--- containers/build-postgresql.sh
+++ containers/build-postgresql.sh
@@ -4,4 +4,7 @@ docker build -t postgresql:9.4 postgresql/9.4/.
 docker build -t postgresql:10.12 postgresql/10.12/.
 docker build -t postgresql:11.7 postgresql/11.7/.
 docker build -t postgresql:12.2 postgresql/12.2/.
-
+docker tag postgresql:9.4 brechtian-docker-images.bintray.io/brecht/postgresql:9.4
+docker tag postgresql:10.12 brechtian-docker-images.bintray.io/brecht/postgresql:10.12
+docker tag postgresql:11.7 brechtian-docker-images.bintray.io/brecht/postgresql:11.7
+docker tag postgresql:12.2 brechtian-docker-images.bintray.io/brecht/postgresql:12.2
