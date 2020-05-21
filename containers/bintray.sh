430ee08d56b3322ab565b62d56ec4da5f5146cee
lteqgt <sebastian.harko@protonmail.com>
2020-05-20 22:21:02 -0700

    attempting to fix travis build (#25)

--- containers/bintray.sh
+++ containers/bintray.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 docker login -u sebastianharko -p $BINTRAY_API_KEY brechtian-docker-images.bintray.io
-docker tag postgresql:9.4 brechtian-docker-images.bintray.io/brecht/postgresql:9.4
-docker tag postgresql:10.12 brechtian-docker-images.bintray.io/brecht/postgresql:10.12
-docker tag postgresql:11.7 brechtian-docker-images.bintray.io/brecht/postgresql:11.7
-docker tag postgresql:12.2 brechtian-docker-images.bintray.io/brecht/postgresql:12.2
+
 docker push brechtian-docker-images.bintray.io/brecht/postgresql:9.4
 docker push brechtian-docker-images.bintray.io/brecht/postgresql:10.12
 docker push brechtian-docker-images.bintray.io/brecht/postgresql:11.7
 docker push brechtian-docker-images.bintray.io/brecht/postgresql:12.2
+
+docker tag brecht:0.1 brechtian-docker-images.bintray.io/brecht/brecht:0.1
+docker push brechtian-docker-images.bintray.io/brecht/brecht:0.1
